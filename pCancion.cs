e1b739b045c853395a096f03f19a08ff4f0f7145
RichardVargas <30979624+RichardVargas@users.noreply.github.com>
2017-08-16 13:43:17 -0500

    Add files via upload

---
+++ pCancion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WebServiceRES
+{
+    class pCancion
+    {
+        public string action { get; set; }
+        public pCancion()
+        {
+            action = "Motos";
+        }
+    };
+
+    class resultsCanciones
+    {
+        public int idMoto{ get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public int Costo { get; set; }
+        public string Existencia { get; set; }
+        public override string ToString()
+        {
+            return String.Format("{0} : {1}  :{2}   :{3} ",Nombre,Marca,Costo,Existencia);
+        }
+    };
+}
---
+++ pGenero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WebServiceRES
+{
+    class pGenero
+    {
+
+        public string action { get; set; }
+        public string idMoto { get; set; }
+        public string idCliente { get; set; }
+
+
+
+        public pGenero(string idMotos, string idClientes )
+        {
+            action = "venta";
+            idMoto = idMotos;
+            idCliente = idClientes;
+        }
+    };
+
+    
+}
